fdc34030050b423158f039a82d763b95ceab77b1
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-06-23 06:59:46 +0200

    chore: Add type-enum to commitlint config

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -5,5 +5,23 @@ export default {
     'body-max-line-length': [0, 'always'],
     'footer-max-line-length': [0, 'always'],
     'subject-case': [0, 'always'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'deps',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
   },
 };
